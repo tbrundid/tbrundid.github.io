857db0ff318e144c887f9b1ecfe595156c0e27f3
tbrundid <41038196+tbrundid@users.noreply.github.com>
2018-08-09 13:02:34 -0500

    test

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+<?php tag error_reporting(E_ALL); ini_set('display_errors', 1);
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -14,7 +15,7 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-method= post
+method= "post""
 // Create the email and send the message
 $to = 'troybrundidge@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
